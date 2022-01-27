b5630af0490f63eaf26e90fc6efedcfbfed42751
Tamme Schichler <tamme@schichler.dev>
2022-01-27 17:39:54 +0100

    Support `#![deallocate_owned]` for unsized original types too

--- src/__.rs
+++ src/__.rs
@@ -1,11 +1,18 @@
 #![doc(hidden)]
 
-use core::mem::MaybeUninit;
+use core::{cell::UnsafeCell, mem::ManuallyDrop};
 
 #[cfg(feature = "macros")]
 pub use static_assertions::const_assert;
 
-pub unsafe fn deallocate_owned<T>(raw: *mut T) {
+/// Deallocates a global-allocator allocation of `T`, iff the `"alloc"` feature is enabled.
+///
+/// # Safety
+///
+/// The target of `raw` currently always must be initialised.
+///
+/// > The requirements can be loosened a bit once the feature [`layout_for_ptr`](https://github.com/rust-lang/rust/issues/69835) is stabilised.
+pub unsafe fn deallocate_owned<T: ?Sized>(raw: *mut T) {
 	#[cfg(feature = "alloc")]
-	drop(::alloc::boxed::Box::<MaybeUninit<T>>::from_raw(raw.cast()))
+	drop(::alloc::boxed::Box::<ManuallyDrop<UnsafeCell<T>>>::from_raw(raw as *mut _))
 }
